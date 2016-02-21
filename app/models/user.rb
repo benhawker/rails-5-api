710e2601f298982605d05401f37f02091f94cd7c
Ben <ben.c.hawker@gmail.com>
2016-02-21 13:07:01 +0800

    Add api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
+	##Callbacks
+	before_create :generate_api_key
+
+	##Associations
 	has_many :properties
+
+	private
+
+	def generate_api_key
+		loop do
+			token = SecureRandom.base64.tr('+/=', 'Qrt')
+			break token unless User.exists?(api_key: token).any?
+		end
+	end
+
 end
+
+
