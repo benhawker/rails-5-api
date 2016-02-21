200f6631383831302aab880885d71d4234a10311
Ben <ben.c.hawker@gmail.com>
2016-02-21 13:42:01 +0800

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first) 
-
-User.create(name: "Bob", email: "bob@gmail.com")
-Property.create(address: "Somewhere", rooms: 2, bathrooms: 3, price_cents: 1150)
+[
+  { name: "Bob", email: "bob@gmail.com"},
+  { name: "Bill", email: "bill@gmail.com"},
+  { name: "Ben", email: "ben@gmail.com"},
+  { name: "Brad", email: "brad@gmail.com"}
+].each do |user_attributes|
+  User.create(user_attributes)
+end
