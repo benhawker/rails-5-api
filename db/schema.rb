577487435ddba7c7ef4997d34b7667dd86d0c75f
Ben <ben.c.hawker@gmail.com>
2016-02-21 19:33:51 +0800

    Add password digest to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221105233) do
+ActiveRecord::Schema.define(version: 20160221113214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,9 +43,10 @@ ActiveRecord::Schema.define(version: 20160221105233) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "api_key"
+    t.string   "password_digest"
   end
 
   add_foreign_key "authentication_tokens", "users"
